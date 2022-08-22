c01cf9160a2b24de68d96713fd70f61ada366e3b
unknown <buragohain4@gmail.com>
2022-08-22 16:40:24 +0530

    Adding UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+        }
+    }
+}
