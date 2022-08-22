58dd30c142b772e6d2147d0b1b06149788f0a2c3
unknown <buragohain4@gmail.com>
2022-08-22 16:49:05 +0530

    Adding UC5

---
+++ UC5/UC5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int WorkingHrs;
+            int DaysperMonth = 20;
+            int perHrWage = 20;
+            int totalWage;
+            Random var = new Random();
+            int empCheck = var.Next(0, 3);
+            switch (empCheck)
+            {
+                case 1:
+                    Console.WriteLine("Employee is Working Full Time");
+                    WorkingHrs = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is Doing Part Time");
+                    WorkingHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    WorkingHrs = 0;
+                    break;
+            }
+            totalWage = WorkingHrs * perHrWage * DaysperMonth;
+            Console.WriteLine("So, Monthly Employee wage is: " + totalWage);
+        }
+    }
+}
