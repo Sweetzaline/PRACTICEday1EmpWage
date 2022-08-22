3b93c2d7a4f7f7815ab30ed1b502b6ac7db67e0b
unknown <buragohain4@gmail.com>
2022-08-22 16:42:30 +0530

    Adding UC2

---
+++ UC2/UC2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullDayHrs = 8;
+            int perHrWage = 20;
+            int totalWage;
+            Random var = new Random();
+            int empCheck = var.Next(0, 2);
+            if(empCheck == 1)
+            {
+                Console.WriteLine("Employee is Present");
+                totalWage = fullDayHrs * perHrWage;
+                Console.WriteLine("One day employee wage is: " + totalWage );
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("No Wage for Absentee.");
+            }
+            
+        }
+    }
+}
