a48855b7f5de55c6af8f3ee2f4987d8b93498628
unknown <buragohain4@gmail.com>
2022-08-22 16:52:32 +0530

    Adding UC8

---
+++ UC8/UC8/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace UC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Dmart= new Class1("Dmart", 20, 3, 100);
+            Class1 Max= new Class1("Max", 40, 3, 400);
+
+            Dmart.CalculateWage();
+            Console.WriteLine(Dmart.ToString());
+            Max.CalculateWage();
+           Console.WriteLine(Max.ToString());
+        }
+    }
+    public class Class1
+    {
+        private string company;
+        private int perHrWage;
+        private int maxWorkingDays;
+        private int maxWorkingHr;
+        private int Wage;
+
+        public Class1(string company, int perHrWage, int maxWorkingDays, int maxWorkingHr)//Copy Constructor
+        {
+            this.company = company;
+            this.perHrWage = perHrWage;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHr = maxWorkingHr;
+        }  
+        public void CalculateWage()
+        {
+            int TotalWorkingHr = 0, WorkingDay = 1, WorkingHrs, totalWage=0;
+
+            while (TotalWorkingHr <= maxWorkingHr && WorkingDay < maxWorkingDays)
+            {
+                Random var = new Random();
+                int empCheck = var.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is Working Full Time");
+                        WorkingHrs = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Doing Part Time");
+                        WorkingHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        WorkingHrs = 0;
+                        break;
+                }
+                Wage = WorkingHrs * perHrWage;
+                Console.WriteLine("Day" + WorkingDay + "Employee wage is:" + Wage + "\n");
+                WorkingDay++;
+                totalWage = totalWage + Wage;
+                TotalWorkingHr = TotalWorkingHr + WorkingHrs;
+            }
+            Console.WriteLine("\nFor " + this.company + " Company, Total employee working hour is:" + TotalWorkingHr + ". & Total wage is:" + totalWage + "\n");
+        }
+    }
+}
