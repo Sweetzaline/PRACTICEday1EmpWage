73c7055f44a219152816a52546fe9cf45302fe58
unknown <buragohain4@gmail.com>
2022-08-22 17:00:28 +0530

    Welcome message for you.

---
+++ MESSAGE/MESSAGE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MESSAGE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Employee Wage Computation Program on Master Branch!******");
+        }
+    }
+}
